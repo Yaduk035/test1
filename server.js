93374ea5808cc91e8c598eb48bc1a22a9742f925
Yaduk035 <yaduk946@gmail.com>
2023-05-28 20:20:42 +0530

    16th commit

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('*', (req, res) => {
 connectDB()
   .then(() => {
     // Start the server
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 4000;
     app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
